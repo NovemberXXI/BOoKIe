38049fec967edd43c16610c8401c40e2baa78664
Edmond Setati <NovemberXXI.Business@gmail.com>
2023-03-10 13:40:53 +0200

    first commit

---
+++ ticket.js
@@ -0,0 +1,26 @@
+const ticket = [
+  {
+    id: 0,
+    name: "General Ticket",
+    price: R150,
+    instock: 100,
+    discription:
+      "This ticket only allows you to get in the general area of the event",
+  },
+  {
+    id: 1,
+    name: "VIP Ticket",
+    price: R150,
+    instock: 50,
+    discription:
+      "This ticket allows you a full experience, with complimentary surprises. not including suite access",
+  },
+  {
+    id: 2,
+    name: "VVIP Ticket",
+    price: R150,
+    instock: 100,
+    discription:
+      "This ticket only allows you full experience, with complimentary surprises, including suite access and celebrity meet and greet",
+  },
+];
